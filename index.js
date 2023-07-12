12ff992f3af7c3ee17340cd7e7e78cde76840ae7
Vitalii Braun <vitossvx@gmail.com>
2023-07-12 20:26:53 +0300

    fix

--- index.js
+++ index.js
@@ -8,19 +8,16 @@ const bot = new TelegramBot(process.env.TGBOT_API_KEY, { polling: true });
 const allowedUsers =
   process.env.ALLOWED_USERS?.replace(" ", "").split(",") || null;
 
+bot.onText("/start", async (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Приветствую! Я ChatGPT, введите ваш вопрос.");
+});
+
 bot.on("message", async (msg) => {
-  if (
-    (allowedUsers?.includes(msg.chat.username) || allowedUsers == null) &&
-    msg.text !== "/start"
-  ) {
+  if (allowedUsers?.includes(msg.chat.username) || allowedUsers == null) {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, "пишу ответ, ждите...");
     const gptResponse = await sendMessageToChatGPT(msg.text);
     bot.sendMessage(chatId, gptResponse);
   }
-});
-
-bot.onText("/start", async (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Приветствую! Я ChatGPT, введите ваш вопрос.");
 });
